dfed60ca71f8a89ade5c94e16de81acfda401e43
JustinAQuan <jaq1401@gmail.com>
2021-05-12 13:56:55 -0700

    made start game text clickable to start play scene

--- src/scene/menu.js
+++ src/scene/menu.js
@@ -9,8 +9,10 @@ class menu extends Phaser.Scene {
     }
 
     create() {
+        let scene = this;
+
         // create homescreen
-        this.homescreen = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'homescreen').setOrigin(0,0);
+        this.homescreen = this.add.sprite(0, 0, 'homescreen').setOrigin(0,0);
         
         // monitor borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0x333333).setOrigin(0 ,0);
@@ -18,10 +20,17 @@ class menu extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0x333333).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0x333333).setOrigin(0 ,0);
 
-        // title
+        // title text (this can be made in combination with menu image)
         this.add.text(game.config.width/2, game.config.height/2, "Horror Game", {fontSize: "50px", color: 0xffffff}).setOrigin(.5,.5);
+
+        // play text (this can be made into an image instead)
+        this.play = this.add.text(game.config.width/2, game.config.height/1.5, "Start Game", {fontSize: "30px", color: 0xffffff}).setOrigin(.5,.5);
+        this.play.setInteractive({ cursor: 'pointer' });
     
-        // go to play scene
-        this.scene.start("playScene");
+        // makes play text clickable
+        this.play.on('pointerdown', function () {
+            scene.scene.sleep("menuScene");
+            scene.scene.start("playScene");
+        });
     }
 }
