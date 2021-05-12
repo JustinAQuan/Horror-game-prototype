7c8c6679c216e33b9535c395f1903c628c9c429c
JustinAQuan <jaq1401@gmail.com>
2021-05-11 19:07:27 -0700

    made monitor scale down when pressing space

--- src/scene/play.js
+++ src/scene/play.js
@@ -20,16 +20,11 @@ class play extends Phaser.Scene {
         // this.outside = this.add.tileSprite().setOrigin(0,0);
 
         // creates room
-        this.room = this.add.tileSprite(-game.config.width / 5, -game.config.height / 20, 1200, 900, 'room').setOrigin(0,0);
+        this.room = this.add.sprite(-game.config.width / 5, -game.config.height / 20, 'room').setOrigin(0,0);
 
         // create homescreen
-        this.homescreen = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'homescreen').setOrigin(0,0);
-
-        // monitor borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize + 10, 0xd4d4b8).setOrigin(0 ,0);
-        this.add.rectangle(0, game.config.height - borderUISize - 10, game.config.width, borderUISize + 10, 0xd4d4b8).setOrigin(0 ,0);
-        this.add.rectangle(0, 0, borderUISize + 10, game.config.height, 0xd4d4b8).setOrigin(0 ,0);
-        this.add.rectangle(game.config.width - borderUISize - 10, 0, borderUISize + 10, game.config.height, 0xd4d4b8).setOrigin(0 ,0);
+        this.homescreen = this.add.sprite(0, 0, 'homescreen').setOrigin(0,0);
+        this.homescreen.setScale(.61);
 
         // have main camera follow mouse inputs
         this.cameras.main.startFollow(this.input, false, 0.01, 0.01);
@@ -45,6 +40,10 @@ class play extends Phaser.Scene {
                 -game.config.height / 20,   // y: -30
                 game.config.width * 1.5,    // width: 1200 (therefore, can scroll right until 1040 pixels)
                 game.config.height * 1.1);  // height: 660 (therefore, can scroll down until 630 pixels)
+            
+            this.homescreen.setX(-50);
+            this.homescreen.setY(150);
+            this.homescreen.setScale(.5);
         }
         else{   // can only see screen monitor when not pressing space
             this.cameras.main.setBounds(
@@ -52,6 +51,10 @@ class play extends Phaser.Scene {
                 0,                      // y: 0
                 game.config.width,      // width: 800
                 game.config.height);    // height: 600
+
+            this.homescreen.setX(0);
+            this.homescreen.setY(0);
+            this.homescreen.setScale(.61);
         }
 
         //update outside sprite to creepy stuff when stuff happens
