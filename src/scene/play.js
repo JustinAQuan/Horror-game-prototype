af1d78358854cc8eba77d7681b173859a7fc0209
T-98 <dkhare@ucsc.edu>
2021-05-14 19:04:04 -0700

    added clickable prefab

--- src/scene/play.js
+++ src/scene/play.js
@@ -3,7 +3,7 @@ class play extends Phaser.Scene {
         super("playScene");
     }
 
-    init(data){
+    init(data) {
         this.username = data.username;
     }
 
@@ -29,19 +29,21 @@ class play extends Phaser.Scene {
         // this.outside = this.add.tileSprite().setOrigin(0,0);
 
         // creates room
-        this.room = this.add.sprite(-game.config.width / 5, -game.config.height / 20, 'room').setOrigin(0,0);
+        this.room = this.add.sprite(-game.config.width / 5, -game.config.height / 20, 'room').setOrigin(0, 0);
 
         this.computer = this.add.container();
 
         // create monitor
-        this.monitor_border = this.add.sprite(0, 0, 'monitor_border').setOrigin(0,0);
-        this.homescreen = this.add.sprite(monitorBorderX, monitorBorderY, 'homescreen').setOrigin(0,0);
+        this.monitor_border = this.add.sprite(0, 0, 'monitor_border').setOrigin(0, 0);
+        this.homescreen = this.add.sprite(monitorBorderX, monitorBorderY, 'homescreen').setOrigin(0, 0);
 
         // monitor icons
-        this.myPC = this.add.sprite(monitorBorderX + 10, monitorBorderY + 20, 'my_pc').setOrigin(0,0);
-        this.inbox = this.add.sprite(monitorBorderX + 15, monitorBorderY * 2 + 40, 'inbox').setOrigin(0,0);
-        this.ie = this.add.sprite(monitorBorderX + 15, monitorBorderY * 4 + 20, 'ie').setOrigin(0,0);
-        this.rb = this.add.sprite(monitorBorderX + 15, monitorBorderY * 6 + 20, 'recycle_bin').setOrigin(0,0);
+        this.myPC = this.add.sprite(monitorBorderX + 10, monitorBorderY + 20, 'my_pc').setOrigin(0, 0);
+        this.inbox = this.add.sprite(monitorBorderX + 15, monitorBorderY * 2 + 40, 'inbox').setOrigin(0, 0);
+        this.ie = this.add.sprite(monitorBorderX + 15, monitorBorderY * 4 + 20, 'ie').setOrigin(0, 0);
+        this.rb = this.add.sprite(monitorBorderX + 15, monitorBorderY * 6 + 20, 'recycle_bin').setOrigin(0, 0);
+
+        //this.inbox = new clickable(monitorBorderX + 15, monitorBorderY * 2 + 40, 'inbox').setOrigin(0, 0);
 
         this.computer.add([this.monitor_border, this.homescreen, this.ie, this.rb, this.inbox, this.myPC]);
 
@@ -49,16 +51,15 @@ class play extends Phaser.Scene {
         this.time.delayedCall(3000, () => {
             // add instruction text
             this.instructions = this.add.text(
-                game.config.width / 2, 
-                game.config.height - monitorBorderY * 4, 
-                "Press Space to look around the room", 
-                {fontSize: "30px", color: 0xffffff}
-            ).setOrigin(0.5,0);
+                game.config.width / 2,
+                game.config.height - monitorBorderY * 4,
+                "Press Space to look around the room", { fontSize: "30px", color: 0xffffff }
+            ).setOrigin(0.5, 0);
 
             // play sound
 
             // add something to window
-            this.beegyoshi = this.add.sprite(500, 0, 'test').setOrigin(0,0);
+            this.beegyoshi = this.add.sprite(500, 0, 'test').setOrigin(0, 0);
             this.beegyoshi.setDepth(-1);
         }, null, this);
 
@@ -69,28 +70,26 @@ class play extends Phaser.Scene {
         this.spaceKey = this.input.keyboard.addKey('SPACE');
     }
 
-    update(){
-        if(this.spaceKey.isDown){   // able to look around the room when pressing space
-            this.cameras.main.setBounds(
-                -game.config.width / 5,     // x: -160
-                -game.config.height / 20,   // y: -30
-                game.config.width * 1.5,    // width: 1200 (therefore, can scroll right until 1040 pixels)
-                game.config.height * 1.1);  // height: 660 (therefore, can scroll down until 630 pixels)
-                
-            this.computer.setY(180);    
+    update() {
+        if (this.spaceKey.isDown) { // able to look around the room when pressing space
+            this.cameras.main.setBounds(-game.config.width / 5, // x: -160
+                -game.config.height / 20, // y: -30
+                game.config.width * 1.5, // width: 1200 (therefore, can scroll right until 1040 pixels)
+                game.config.height * 1.1); // height: 660 (therefore, can scroll down until 630 pixels)
+
+            this.computer.setY(180);
             this.computer.setScale(.75);
-            
-            if(this.instructions){
+
+            if (this.instructions) {
                 this.instructions.destroy();
             }
-        }
-        else{   // can only see screen monitor when not pressing space
+        } else { // can only see screen monitor when not pressing space
             this.cameras.main.setBounds(
-                0,                      // x: 0
-                0,                      // y: 0
-                game.config.width,      // width: 800
-                game.config.height);    // height: 600
-            
+                0, // x: 0
+                0, // y: 0
+                game.config.width, // width: 800
+                game.config.height); // height: 600
+
 
             this.monitor_border.setY(0);
             this.homescreen.setX(monitorBorderX);
