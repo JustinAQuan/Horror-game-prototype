5e0d8541a5a2fc48b07dad6ae1e47b9c9a501248
Justin Quan <46581109+JustinAQuan@users.noreply.github.com>
2021-05-25 13:04:52 -0700

    added loading screen to preload scene

--- src/scene/preload.js
+++ src/scene/preload.js
@@ -4,6 +4,55 @@ class preload extends Phaser.Scene {
     }
 
     preload(){
+        let scene = this;
+
+        /////////////////////////////
+        //      LOADING BAR        //
+        /////////////////////////////
+
+        let progressBar = this.add.graphics();
+        let progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(240, 270, 320, 50);
+
+        let loadingText = this.make.text({
+            x: game.config.width / 2,
+            y: game.config.height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+
+        let percentText = this.make.text({
+            x: game.config.width / 2,
+            y: game.config.height / 2 - 5,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+
+        this.load.on('progress', function (value) {
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(250, 280, 300 * value, 30);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            scene.scene.start("menuScene");
+        });
+
+
         /////////////////////////////
         //       MENU SCENE        //
         /////////////////////////////
@@ -110,9 +159,5 @@ class preload extends Phaser.Scene {
         
         // LOADS BEEG YOSHI
         this.load.image('Beeg Yoshi', './assets/art/test.png');
-    }
-
-    create(){
-        this.scene.start("menuScene");
     }
 }
