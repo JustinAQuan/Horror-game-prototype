997f79690a4996ffe089e59196e3240a368306a6
Justin Quan <46581109+JustinAQuan@users.noreply.github.com>
2021-05-28 12:50:21 -0700

    added instructions to the main menu

--- src/scene/menu.js
+++ src/scene/menu.js
@@ -23,6 +23,8 @@ class menu extends Phaser.Scene {
         // create homescreen
         this.monitor = this.add.sprite(0, 0, 'monitor_border').setOrigin(0,0);
         this.homescreen = this.add.sprite(monitorBorderX, monitorBorderY, 'homescreen').setOrigin(0,0);
+        this.folder = new clickable(this, 75, 490, 'folder', "icon").setOrigin(0,0).setScale(.7);
+        this.add.text(73, 530, "Instructions", {fontFamily: 'VT323', fontSize: "12px", color: 0xffffff, resolution: 2}).setOrigin(0,0);
 
         // title text (this can be made in combination with menu image)
         this.title = this.add.text(game.config.width / 2, game.config.height / 2 - 50, Title, {fontFamily: 'VT323', fontSize: "100px", color: 0xffffff}).setOrigin(.5,.5);
@@ -31,6 +33,18 @@ class menu extends Phaser.Scene {
         this.login = this.add.sprite(game.config.width / 2, game.config.height / 1.5, 'login').setOrigin(0.5,0.5);
         this.login.setInteractive({ cursor: 'pointer' });
         this.clicked = false;
+
+        // adds instructions window
+        this.instructCon = this.add.container();
+        this.instructCon.setPosition(2000, 0);
+
+        let text = "Press and hold \"Space\" to look around the room using the mouse.\n\nUse left mouse click to interact.\n\nPress \"ESC\" to Pause the game.\n\nClose this window by clicking on the \"X\" top right."
+
+        this.instructionsWindow = this.add.sprite(game.config.width / 2, game.config.height / 2, 'instruction_window').setOrigin(.5,.5);
+        this.closeInstruct = new clickable(this, 585, 163, 'close_button');
+        this.instructions = this.add.text(380, 234, text, {fontFamily: 'VT323', fontSize: "13px", color: 0xffffff, resolution: 2}).setOrigin(.5,.5);
+
+        this.instructCon.add([this.instructionsWindow,this.closeInstruct,this.instructions]);
 
         // after a 5 second delay
         this.time.delayedCall(4000, function() {
@@ -48,6 +62,14 @@ class menu extends Phaser.Scene {
                 loop: true,
             });
         });
+
+        this.folder.on('pointerdown', function(){
+            scene.instructCon.setPosition(0, 0);
+        });
+
+        this.closeInstruct.on('pointerdown', function(){
+            scene.instructCon.setPosition(2000, 0);
+        })
 
         // when player clicks on login
         this.login.on('pointerdown', function () {
